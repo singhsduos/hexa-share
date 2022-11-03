caed34434d2fd603bcdcb55df963c9d4b8ce901c
singhsduos <neelesh.singh@jalantechnologies.com>
2022-11-03 23:37:48 +0530

    added - DropZoneComponent

---
+++ frontend/src/components/Heading.tsx
@@ -0,0 +1,9 @@
+const Heading = () => {
+  return (
+    <h1 className="my-4 text-3xl font-medium">
+      Got a File? Share it Like a Fake News
+    </h1>
+  );
+};
+
+export default Heading;
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,13 +1,14 @@
-
+import DropZoneComponent from "@components/DropZoneComponent";
+import Heading from "@components/Heading"
+import { useState } from "react";
 
 export default function Home() {
+    const [file, setFile] = useState(null);
   return (
     <div className="flex flex-col items-center justify-center">
-      <h1 className="my-4 text-3xl font-medium">
-        Got a File? Share it Like a Fake News
-      </h1>
+      <Heading />
       <div className="flex flex-col items-center justify-center bg-gray-800 shadow-xl w-96 rounded-xl">
-        {/* dropzone */}
+        <DropZoneComponent setFile={setFile} />
         {/* renderfile */}
         {/* upload button */}
       </div>
