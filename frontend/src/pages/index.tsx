478e89e4a1396a6b305583d538c8a3a09584ac73
singhsduos <neelesh.singh@jalantechnologies.com>
2022-11-03 22:58:00 +0530

    created frontend for main page

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,9 +1,16 @@
-import Delete from "@components/Delete";
+
 
 export default function Home() {
   return (
-    <div className="h-screen bg-gray-800">
-      <Delete />
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="my-4 text-3xl font-medium">
+        Got a File? Share it Like a Fake News
+      </h1>
+      <div className="flex flex-col items-center justify-center bg-gray-800 shadow-xl w-96 rounded-xl">
+        {/* dropzone */}
+        {/* renderfile */}
+        {/* upload button */}
+      </div>
     </div>
   );
 }
