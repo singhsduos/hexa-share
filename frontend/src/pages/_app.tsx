448ab6de5b4b48a6e3b4e989496f927ca975d262
singhsduos <neelesh.singh@jalantechnologies.com>
2022-11-04 12:37:51 +0530

    added - uploadButton

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,7 @@
-import "tailwindcss/tailwind.css";
 import "../../styles/globals.css";
+import axios from "axios";
 
+axios.defaults.baseURL = "http://localhost:8800/";
 function MyApp({ Component, pageProps }) {
     return (
       <div className="grid h-screen font-serif text-white bg-gray-900 place-items-center">
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,17 +1,39 @@
 import DropZoneComponent from "@components/DropZoneComponent";
-import Heading from "@components/Heading"
+import Heading from "@components/Heading";
 import RenderFile from "@components/RenderFile";
+import axios from "axios";
 import { useState } from "react";
 
 export default function Home() {
-    const [file, setFile] = useState(null);
+  const [file, setFile] = useState(null);
+  const [downloadPageLink, setDownloadPageLink] = useState(null);
+  const [id, setId] = useState(null);
+  const [uploadState, setUploadState] = useState<
+    "Uploading" | "Upload Failed" | "Uploaded" | "Upload"
+  >("Upload");
+
+  const handleUpload = async () => {
+    setUploadState("Uploading");
+    const formData = new FormData();
+    formData.append("myFile", file);
+    try {
+      const { data } = await axios.post("/api/files/upload", formData);
+      setUploadState("Uploaded");
+      setDownloadPageLink(data.downloadPageLink);
+      setId(data.id);
+    } catch (error) {
+      console.log(error.response.data);
+      setUploadState("Upload Failed");
+    }
+  };
+
   return (
     <div className="flex flex-col items-center justify-center">
       <Heading />
       <div className="flex flex-col items-center justify-center bg-gray-800 shadow-xl w-96 rounded-xl">
         {/* Drag and Drop Componenet */}
         <DropZoneComponent setFile={setFile} />
-        
+
         {/* renderfile */}
         {file && (
           <RenderFile
@@ -24,6 +46,25 @@ export default function Home() {
         )}
 
         {/* upload button */}
+        {file && (
+          <button
+            disabled={
+              uploadState === "Uploading" || uploadState === "Uploaded"
+                ? true
+                : false
+            }
+            className="button"
+            onClick={handleUpload}
+            style={{
+              cursor:
+                uploadState === "Uploading" || uploadState === "Uploaded"
+                  ? "not-allowed"
+                  : "pointer",
+            }}
+          >
+            {uploadState}
+          </button>
+        )}
       </div>
     </div>
   );
