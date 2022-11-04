bc30ae688af69fc7d5f1ecd6048ff445da9bf47a
singhsduos <neelesh.singh@jalantechnologies.com>
2022-11-04 22:23:31 +0530

    update - metaTag

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -2,6 +2,7 @@ import "../../styles/globals.css";
 import axios from "axios";
 import { transitions, positions, Provider as AlertProvider } from "react-alert";
 import AlertTemplate from "react-alert-template-basic";
+import Head from "next/head";
 
 axios.defaults.baseURL = "https://hexashare.onrender.com/";
 // Alert Message CSS
@@ -15,6 +16,9 @@ const options = {
 function MyApp({ Component, pageProps }) {
   return (
     <AlertProvider template={AlertTemplate} {...options}>
+      <Head>
+        <title>HexaShare</title>
+      </Head>
       <div className="grid h-screen font-serif text-white bg-gray-900 place-items-center">
         <div>
           <Component {...pageProps} />
