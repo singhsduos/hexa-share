9169751dae68b4985665d2ec3920f8123cc86ce8
singhsduos <neelesh.singh@jalantechnologies.com>
2022-11-04 11:29:05 +0530

    added - render file component

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,5 +1,6 @@
 import DropZoneComponent from "@components/DropZoneComponent";
 import Heading from "@components/Heading"
+import RenderFile from "@components/RenderFile";
 import { useState } from "react";
 
 export default function Home() {
@@ -8,8 +9,20 @@ export default function Home() {
     <div className="flex flex-col items-center justify-center">
       <Heading />
       <div className="flex flex-col items-center justify-center bg-gray-800 shadow-xl w-96 rounded-xl">
+        {/* Drag and Drop Componenet */}
         <DropZoneComponent setFile={setFile} />
+        
         {/* renderfile */}
+        {file && (
+          <RenderFile
+            file={{
+              format: file.type.split("/")[1],
+              name: file.name,
+              sizeInBytes: file.size,
+            }}
+          />
+        )}
+
         {/* upload button */}
       </div>
     </div>
