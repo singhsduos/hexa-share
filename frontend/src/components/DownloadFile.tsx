66fc785a0e2bc0134aacc683e756215724c36926
singhsduos <neelesh.singh@jalantechnologies.com>
2022-11-04 20:07:05 +0530

    added - alert toaster

--- frontend/src/components/DownloadFile.tsx
+++ frontend/src/components/DownloadFile.tsx
@@ -1,4 +1,8 @@
+import { useAlert } from "react-alert";
+
 const DownloadFile = ({ downloadPageLink }) => {
+  const alert = useAlert();
+
   return (
     <div className="p-1">
       <h1 className="my-2 text-lg font-medium">
@@ -11,7 +15,7 @@ const DownloadFile = ({ downloadPageLink }) => {
           src="/images/copy.png"
           alt=""
           className="object-contain w-8 h-8 cursor-pointer"
-          onClick={() => navigator.clipboard.writeText(downloadPageLink)}
+          onClick={() => { navigator.clipboard.writeText(downloadPageLink);   alert.success("Successfully, Link Copied!");}}
         />
       </div>
     </div>
--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,15 +1,27 @@
 import "../../styles/globals.css";
 import axios from "axios";
+import { transitions, positions, Provider as AlertProvider } from "react-alert";
+import AlertTemplate from "react-alert-template-basic";
 
 axios.defaults.baseURL = "http://localhost:8800/";
+// Alert Message CSS
+const options = {
+  timeout: 5000,
+  position: positions.BOTTOM_CENTER,
+  transition: transitions.SCALE,
+  
+}
+
 function MyApp({ Component, pageProps }) {
-    return (
+  return (
+    <AlertProvider template={AlertTemplate} {...options}>
       <div className="grid h-screen font-serif text-white bg-gray-900 place-items-center">
         <div>
           <Component {...pageProps} />
         </div>
       </div>
-    );
+    </AlertProvider>
+  );
 }
 
 export default MyApp;
